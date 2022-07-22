6218b1799b473c2148977bcc2dd8778172a0b61b
Caio-Santana <caio.santana@live.com>
2022-07-22 09:38:24 -0300

    Add dark-mode

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+const modeBtn = document.querySelector(".toggle-mode");
+
+modeBtn.addEventListener("click", (event) => {
+  const body = document.querySelector("body");
+  body.classList.toggle("dark-mode");
+  modeBtn.textContent =
+    modeBtn.textContent === "dark_mode" ? "light_mode" : "dark_mode";
+  event.stopPropagation();
+});
+
 function add() {}
 
 function subtract() {}
